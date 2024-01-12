af7eaa2c7f863d5ad57893e0bf5c791da5147927
joshua chivile <joshuachivile@gmail.com>
2024-01-12 08:16:25 +0300

    get requests with python

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,16 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        dict_list = []
+        contents = json.loads(self.get_response_body())
+        for content in contents:
+            dict_list.append(content)
+        return dict_list
+
+getty = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+#print(getty.get_response_body())
+print(getty.load_json())
